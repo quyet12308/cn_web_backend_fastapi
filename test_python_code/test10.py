b59b03e3012a03e36e466fddb2350ab404f39583
quyet <quyet12308@gmail.com>
2023-10-13 22:15:45 +0700

    fix api key

--- test_python_code/test10.py
+++ test_python_code/test10.py
@@ -70,13 +70,14 @@
 
 
 import requests
+from base_code.security_info import api_key
 
 url = "https://travel-advisor.p.rapidapi.com/restaurants/list-by-latlng"
 
 querystring = {"latitude":"12.91285","longitude":"100.87808","limit":"30","currency":"USD","distance":"2","open_now":"false","lunit":"km","lang":"en_US"}
 
 headers = {
-	"X-RapidAPI-Key": "93d691634emsh06aece34ef9f62ap168b08jsn94ecdbe3a334",
+	"X-RapidAPI-Key": f"{api_key['X-RapidAPI-Key']}",
 	"X-RapidAPI-Host": "travel-advisor.p.rapidapi.com"
 }
 
